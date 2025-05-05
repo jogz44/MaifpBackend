387e516752e6b4547a118c8d97b56bf88373a376
Neil Benjamin Roble <neilbenjaminroble@gmail.com>
2025-05-05 16:22:42 +0800

    started the export data functionality

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+// use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 
 return Application::configure(basePath: dirname(__DIR__))
